4d370be7a6bd89d54a7bdaf63a10a34c944c4b74
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2022-01-24 19:47:26 +0100

    Initial commit

---
+++ src/main/java/sda/pl/zdjavapol96/EndProjectWarehouseManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package sda.pl.zdjavapol96;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndProjectWarehouseManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndProjectWarehouseManagmentSystemApplication.class, args);
+    }
+
+}
