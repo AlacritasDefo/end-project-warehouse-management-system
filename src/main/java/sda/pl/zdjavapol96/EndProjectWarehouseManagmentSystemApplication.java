be9a155958b79507b7e13b70d435748dfcd6b7a4
AlacritasDefo <arkadiuszprzychocki@gmail.com>
2022-01-25 18:20:46 +0100

    dependency add

--- src/main/java/sda/pl/zdjavapol96/EndProjectWarehouseManagmentSystemApplication.java
+++ src/main/java/sda/pl/zdjavapol96/EndProjectWarehouseManagmentSystemApplication.java
@@ -1,13 +1,25 @@
 package sda.pl.zdjavapol96;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sda.pl.zdjavapol96.repository.UserAppRepository;
+import sda.pl.zdjavapol96.service.UserAppService;
 
 @SpringBootApplication
-public class EndProjectWarehouseManagmentSystemApplication {
+public class EndProjectWarehouseManagmentSystemApplication implements CommandLineRunner {
+    private UserAppRepository userAppRepository;
+
+    public EndProjectWarehouseManagmentSystemApplication(){
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EndProjectWarehouseManagmentSystemApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
